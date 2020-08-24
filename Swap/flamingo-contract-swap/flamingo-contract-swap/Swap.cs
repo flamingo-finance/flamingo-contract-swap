c0f286473c93f727513ae809dbdec117ad70e26b
晨 黎 <lichen@neo.org>
2020-08-24 15:20:10 +0800

    add dependency

---
+++ Swap/flamingo-contract-swap/flamingo-contract-swap/Swap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services;
+
+
+namespace flamingo_contract_swap
+{
+    public partial class Swap : SmartContract
+    {
+    }
+}
