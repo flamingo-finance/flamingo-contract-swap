b72740c8f4605b456dff4ea28c5af011d49a7a34
txhsl <799498265@qq.com>
2022-06-09 15:44:23 +0800

    init new router

--- Swap/flamingo-contract-swap/FlamingoSwapRouter/FlamingoSwapRouterContract.Admin.cs
+++ Swap/flamingo-contract-swap/FlamingoSwapRouter/FlamingoSwapRouterContract.Admin.cs
@@ -10,11 +10,14 @@ namespace FlamingoSwapRouter
     public partial class FlamingoSwapRouterContract
     {
         #region Admin
-        [InitialValue("NdDvLrbtqeCVQkaLstAwh3md8SYYwqWRaE", ContractParameterType.Hash160)]
+        [InitialValue("NdrUjmLFCmr6RjM52njho5sFUeeTdKPxG9", ContractParameterType.Hash160)]
         static readonly UInt160 superAdmin = default;
 
-        [InitialValue("0xca2d20610d7982ebe0bed124ee7e9b2d580a6efc", ContractParameterType.Hash160)]
+        [InitialValue("0xc0695bdb8a87a40aff33c73ff6349ccc05fa9f01", ContractParameterType.Hash160)]
         static readonly UInt160 Factory = default;
+
+        [InitialValue("0x0b336d0dd0f8eb99f7d818d3f6f74d6d018851c9", ContractParameterType.Hash160)]
+        static readonly UInt160 OrderBook = default;
 
         const string AdminKey = nameof(superAdmin);
 
--- Swap/flamingo-contract-swap/FlamingoSwapRouter/FlamingoSwapRouterContract.Helper.cs
+++ Swap/flamingo-contract-swap/FlamingoSwapRouter/FlamingoSwapRouterContract.Helper.cs
@@ -54,6 +54,111 @@ namespace FlamingoSwapRouter
             return (UInt160)pairContract;
         }
 
+        /// <summary>
+        /// 查询TokenA,TokenB交易对合约的里的持有量并按A、B顺序返回
+        /// </summary>
+        /// <param name="tokenA"></param>
+        /// <param name="tokenB"></param>
+        /// <returns></returns>
+        public static BigInteger[] GetReserves(UInt160 tokenA, UInt160 tokenB)
+        {
+            var reserveData = (ReservesData)Contract.Call(GetExchangePairWithAssert(tokenA, tokenB), "getReserves", CallFlags.ReadOnly, new object[] { });
+            return tokenA.ToUInteger() < tokenB.ToUInteger() ? new BigInteger[] { reserveData.Reserve0, reserveData.Reserve1 } : new BigInteger[] { reserveData.Reserve1, reserveData.Reserve0 };
+        }
+
+        /// <summary>
+        /// 给定一个价格区间，查询限价簿剩余不能满足的输入量和能够交易的输出量
+        /// </summary>
+        /// <param name="tokenA"></param>
+        /// <param name="tokenB"></param>
+        /// <param name="startPrice"></param>
+        /// <param name="endPrice"></param>
+        /// <param name="amountOutMin"></param>
+        public static BigInteger[] GetOrderBookAmountOut(UInt160 tokenA, UInt160 tokenB, BigInteger startPrice, BigInteger endPrice, BigInteger amountOutMin)
+        {
+            return (BigInteger[])Contract.Call(OrderBook, "getAmountOut", CallFlags.All, new object[] { tokenA, tokenB, startPrice, endPrice, amountOutMin });
+        }
+
+        /// <summary>
+        /// 给定一个价格区间，查询限价簿剩余不能满足的输出量和能够交易的输入量
+        /// </summary>
+        /// <param name="tokenA"></param>
+        /// <param name="tokenB"></param>
+        /// <param name="startPrice"></param>
+        /// <param name="endPrice"></param>
+        /// <param name="amountInMax"></param>
+        public static BigInteger[] GetOrderBookAmountIn(UInt160 tokenA, UInt160 tokenB, BigInteger startPrice, BigInteger endPrice, BigInteger amountInMax)
+        {
+            return (BigInteger[])Contract.Call(OrderBook, "getAmountIn", CallFlags.All, new object[] { tokenA, tokenB, startPrice, endPrice, amountInMax });
+        }
+
+        /// <summary>
+        /// 向限价簿获取交易对的最优报价
+        /// </summary>
+        /// <param name="tokenA"></param>
+        /// <param name="tokenB"></param>
+        /// <param name="isBuy"></param>
+        public static BigInteger GetOrderBookPrice(UInt160 tokenA, UInt160 tokenB, bool isBuy)
+        {
+            return (BigInteger)Contract.Call(OrderBook, "getMarketPrice", CallFlags.ReadOnly, new object[] { tokenA, tokenB, isBuy });
+        }
+
+        /// <summary>
+        /// 向限价簿获取交易对的下一级报价
+        /// </summary>
+        /// <param name="tokenA"></param>
+        /// <param name="tokenB"></param>
+        /// <param name="isBuy"></param>
+        /// <param name="price"></param>
+        public static BigInteger GetOrderBookNextPrice(UInt160 tokenA, UInt160 tokenB, bool isBuy, BigInteger price)
+        {
+            return (BigInteger)Contract.Call(OrderBook, "getNextPrice", CallFlags.ReadOnly, new object[] { tokenA, tokenB, isBuy, price });
+        }
+
+        /// <summary>
+        /// 向限价簿获取交易对的报价基准代币
+        /// </summary>
+        /// <param name="tokenA"></param>
+        /// <param name="tokenB"></param>
+        public static UInt160 GetBaseToken(UInt160 tokenA, UInt160 tokenB)
+        {
+            return (UInt160)Contract.Call(OrderBook, "getBaseToken", CallFlags.ReadOnly, new object[] { tokenA, tokenB });
+        }
+
+        /// <summary>
+        /// 向限价簿获取交易对的报价精度
+        /// </summary>
+        /// <param name="tokenA"></param>
+        /// <param name="tokenB"></param>
+        public static int GetQuoteDecimals(UInt160 tokenA, UInt160 tokenB)
+        {
+            return (int)Contract.Call(OrderBook, "getQuoteDecimals", CallFlags.ReadOnly, new object[] { tokenA, tokenB });
+        }
+
+        /// <summary>
+        /// 向限价簿转发市场交易请求
+        /// </summary>
+        /// <param name="tokenA"></param>
+        /// <param name="tokenB"></param>
+        /// <param name="startPrice"></param>
+        /// <param name="endPrice"></param>
+        /// <param name="amountInMax"></param>
+        private static BigInteger SendMarketOrder(UInt160 tokenA, UInt160 tokenB, UInt160 sender, bool isBuy, BigInteger price, BigInteger amount)
+        {
+            return (BigInteger)Contract.Call(OrderBook, "dealMarketOrder", CallFlags.All, new object[] { tokenA, tokenB, sender, isBuy, price, amount });
+        }
+
+        /// <summary>
+        /// 向资金池转发兑出请求
+        /// </summary>
+        /// <param name="pairContract"></param>
+        /// <param name="amount0Out"></param>
+        /// <param name="amount1Out"></param>
+        /// <param name="toAddress"></param>
+        private static void SwapOut(UInt160 pairContract, BigInteger amount0Out, BigInteger amount1Out, UInt160 toAddress)
+        {
+            Contract.Call(pairContract, "swap", CallFlags.All, new object[] { amount0Out, amount1Out, toAddress, null });
+        }
 
         /// <summary>
         /// 安全转账，失败则中断退出
